c46e630a8d5dae237d1a18cd25a02c5b355e38f7
Samir Hassen <fake@email.com>
2026-04-20 01:53:03 -0500

    use config in overlays

--- server/routes/status.py
+++ server/routes/status.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 
+from ..config import config as _config
 from ..state import state
 from ..ws_manager import manager
 
@@ -15,3 +16,8 @@ async def get_status() -> dict:
         "discord_members": state.discord_members,
         "ws_connections": manager.count,
     }
+
+
+@router.get("/config")
+async def get_config() -> dict:
+    return {k: v for k, v in _config.items() if k != "server"}
